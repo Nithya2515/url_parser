28ae2c42ae30ae9b58a4f86553ad6b0fb223b597
Nithyanand <nhnithyanand@gmail.com>
2020-08-31 21:09:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const metafetch = require('metafetch');
+const dns = require('dns');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/", async (req,res) => {
+    try{
+        const data =  await mData(req.body.url)
+        res.send(parseMeta(data, req.body.name));
+    }catch(err){
+        res.send('Error')
+    }
+});
+app.post("/dns", (req,res) => {
+    
+const hostName = new URL(req.body.url);
+const a2 = hostName.hostname;
+    dns.resolveTxt(a2, (err, records) => {
+        console.log(records);
+            if(records.flat().indexOf(req.body.txt) >0)
+            {
+            res.send(req.body.txt)
+            }
+    });   
+});
+
+const givenUrl = (url)=>{
+    const hostName = new URL(url);
+    const a2 = hostName.hostname;
+    return dns.resolveTxt(a2, (err, records) => {return records});
+};
+
+
+
+const mData = (url) => {
+    return metafetch.fetch(url, { 
+    userAgent: "User Agent/Defaults to Firefox 58",
+    flags: { 
+        images: false,
+        links: false,
+        language: false
+    },
+    http: {
+        timeout: 30000,
+        headers: {
+            Accept: "*/*"
+        }
+    }
+}).then(meta => {return meta});
+
+};
+
+const parseMeta = (metaData, metaName) => {
+    if (metaData.hasOwnProperty(metaName)){
+        return metaData[metaName]
+    }
+};
+
+app.listen(4000, () => {
+    console.log ('server started')
+});
+
+
